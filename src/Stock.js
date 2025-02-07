f8d3894d36906a20f45b736db276c3f8d00718c9
Cédric <86252008+popille@users.noreply.github.com>
2025-02-07 12:26:32 +0100

    ajout des tests pour le retrait, et le rapport

--- src/Stock.js
+++ src/Stock.js
@@ -8,7 +8,7 @@ function getQuantityOf(article) {
 
 }
 
-function withdrawQuantityOf(article) {
+function withdrawQuantityOf(article, quantity) {
 
 }
 
--- test/Stock.test.js
+++ test/Stock.test.js
@@ -14,9 +14,46 @@ describe("En tant qu’utilisateur, je souhaite consulter la quantité d’un ar
 
 })
 
-describe("En tant qu’utilisateur, je souhaite retirer un article au stock : ", () => {
+describe("En tant qu’utilisateur, je souhaite retirer une quantité donnée sur un stock : ", () => {
+    test("Si l’élément demandé n’est pas un article, j’obtiens une erreur", () => {
+        let article = { name: "ofje", stock: 15 }
+        expect(() => withdrawQuantityOf(article, 1)).toThrow("Un article est attendu")
+
+    })
+
+    test("Si la quantité à retirer est supérieur au stock, j’obtiens une erreur", () => {
+        let article = new Article("pomme", 15)
+        expect(() => withdrawQuantityOf(article, 16)).toThrow("Stock insuffisant")
+    })
+    test("Si la quantité à retirer est négative, j’obtiens une erreur", () => {
+        let article = new Article("pomme", 15)
+        expect(() => withdrawQuantityOf(article, -10)).toThrow("Impossible de retirer une quantité négative")
+    })
+    test("Si la quantité à retirer est de 0, j’obtiens une erreur", () => {
+        let article = new Article("pomme", 15)
+        expect(() => withdrawQuantityOf(article, 0)).toThrow("Impossible de retirer 0")
+    })
+    test("Si la quantité à retirer n'est pas un entier, j’obtiens une erreur", () => {
+        let article = new Article("pomme", 15)
+        expect(() => withdrawQuantityOf(article, 1.5)).toThrow("Impossible de retirer 0")
+    })
+    test("Si la quantité à retirer est inférieur au stock, je retire la quantité au stock", () => {
+        let article = new Article("pomme", 15)
+        expect(withdrawQuantityOf(article, 5)).toBe(new Article("pomme", 10))
+    })
 
 })
 describe("En tant qu’utilisateur, je souhaite obtenir un rapport sur les stocks restants : ", () => {
-
+    test("Si aucune liste d’article n’est fournie, j’obtiens une erreur", () => {
+        let lstArticle = []
+        expect(() => getReport("fe,v")).toThrow("Un liste d'article est attendu")
+    })
+    test("Si aucune liste d’article n’est fournie, j’obtiens une erreur", () => {
+        let lstArticle = []
+        expect(() => getReport(lstArticle)).toThrow("Un liste est vide")
+    })
+    test("Si la liste contient des articles, afficher le nom et la quantité de chaque article ", () => {
+        let lstArticle = [new Article("pomme", 15), new Article("poire", 10)]
+        expect(getReport(lstArticle)).toMatch("/pomme 15; poire 10 /")
+    })
 })
