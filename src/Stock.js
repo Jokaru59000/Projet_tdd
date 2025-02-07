979c915c3c69660d2a793f2037d2a5e7444bf5f0
Cédric <86252008+popille@users.noreply.github.com>
2025-02-07 11:47:30 +0100

    Création de la structure de base du code pour développer

---
+++ src/Stock.js
@@ -0,0 +1,24 @@
+const Article = require("../src/Article")
+
+function addArticle(article, quantity) {
+
+}
+
+function getQuantityOf(article) {
+
+}
+
+function withdrawQuantityOf(article) {
+
+}
+
+function getReport() {
+    let warningLimit = 10
+}
+
+module.exports = {
+    addArticle,
+    getQuantityOf,
+    withdrawQuantityOf,
+    getReport
+}
---
+++ test/Stock.test.js
@@ -0,0 +1,22 @@
+const Article = require("../src/Article")
+const {
+    addArticle,
+    getQuantityOf,
+    withdrawQuantityOf,
+    getReport
+} = require("../src/Stock")
+
+describe("En tant qu’utilisateur, je souhaite ajouter un article au stock : ", () => {
+
+})
+
+describe("En tant qu’utilisateur, je souhaite consulter la quantité d’un article : ", () => {
+
+})
+
+describe("En tant qu’utilisateur, je souhaite retirer un article au stock : ", () => {
+
+})
+describe("En tant qu’utilisateur, je souhaite obtenir un rapport sur les stocks restants : ", () => {
+
+})
