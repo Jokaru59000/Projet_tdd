7caa210cd2fa93c85da830eb711c1691937770b6
Arnaud.m <45336018+Jokaru59000@users.noreply.github.com>
2025-02-07 12:24:35 +0100

    Ajout des tests sur la fonction Ajout un article

--- test/Stock.test.js
+++ test/Stock.test.js
@@ -7,6 +7,34 @@ const {
 } = require("../src/Stock")
 
 describe("En tant qu’utilisateur, je souhaite ajouter un article au stock : ", () => {
+    test("Si ce n’est pas un article, j’obtiens une erreur", () => {
+        let article = {name :"azerty", stock:123}
+        expect(()=>addArticle(article,1)).toThrow("Ce n'est pas un article.")
+    });
+    test("Si la quantité à ajouter n’est pas défini, j'obtiens une erreur", () => {
+        let article = new Article("Planche", 1)
+        expect(()=>addArticle(article,undefined)).toThrow("La quantité à ajouter à ajouter n'est pas définie.")
+    })
+    test("Si la quantité à ajouter n’est pas un nombre, j’obtiens une erreur", () => {
+        let article = new Article("Livre",10)
+        expect(()=>addArticle(article,"A")).toThrow("La quantité à ajouter doit être un nombre.")
+    })
+    test("Si la quantité à ajouter n’est pas un entier, j’obtiens une erreur", () => {
+        let article = new Article("Livre",10)
+        expect(()=>addArticle(article,1.5)).toThrow("La quantité à ajouter doit être un entier.")
+    })
+    test("Si la quantité à ajouter est de 0, j’obtiens un erreur", () => {
+        let article = new Article("Livre",10)
+        expect(()=>addArticle(article,0)).toThrow("La quantité à ajouter doit être supérieur à 0.")
+    })
+    test("Si la quantité à ajouter est négative, j’obtiens une erreur", () => {
+        let article = new Article("Livre",10)
+        expect(()=>addArticle(article,-8)).toThrow("La quantité à ajouter doit être une valeure entière et positive")
+    })
+    test("Si la quantité à ajouter est superieur à 0, la quantité est ajoutée au stock", () => {
+        let article = new Article("Livre",10)
+        expect(addArticle(article,5)).toEqual(new Article("Livre",15))
+    })
 
 })
 
